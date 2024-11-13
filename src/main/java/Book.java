78bd78b50c2223500457e6ecb0d874861ccc60f0
Judith12w2 <judith12w2@gmail.com>
2024-11-13 11:40:47 +0100

    Configuramos inicialmente el Book.java

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -2,3 +2,49 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
+public class Book implements Serializable {
+
+    // Añadimos los atributos necesarios
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    // Constructor vacio por si acaso (Que a veces me falla)
+    public Book() {}
+
+    // Getters y Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{title='" + title + "', author='" + author + "', year=" + year + "}";
+    }
+}
